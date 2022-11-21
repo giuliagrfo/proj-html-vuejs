9c2b713bfc3a0ea1cc915ecf4719f0b41fef7ece
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-21 12:21:11 +0100

    initial commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'AppFooter'
+}
+
+</script>
+
+<template>
+
+
+</template>
+
+<style lang="scss">
+@use '../assets/scss/partials/variables' as*;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+
+</script>
+
+<template>
+
+
+</template>
+
+<style lang="scss">
+@use '../assets/scss/partials/variables' as*;
+</style>
