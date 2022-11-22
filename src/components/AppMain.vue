7fa3c98e9bdf119f80be19014532e9b62b54a25d
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-22 13:39:26 +0100

    completed section -results

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -182,6 +182,48 @@ export default {
                 </div>
             </div>
         </section>
+
+        <section class="results">
+            <div class="container">
+                <span class="tag">What we are doing</span>
+                <h2 class="mb-5"><span class="rounded-1">Results</span> in Numbers</h2>
+                <div class="row justify-content-evenly">
+                    <div class="col-2">
+                        <div class="number">
+                            <h5>128</h5>
+                        </div>
+                        <div class="info">
+                            <h6>Certifications</h6>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="number">
+                            <h5>230</h5>
+                        </div>
+                        <div class="info">
+                            <h6>Employees</h6>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="number">
+                            <h5>517</h5>
+                        </div>
+                        <div class="info">
+                            <h6>Customers</h6>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="number">
+                            <h5>94</h5>
+                        </div>
+                        <div class="info">
+                            <h6>Countries Served</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
     </main>
 
 
@@ -322,6 +364,33 @@ ul {
 
         h6 {
             margin: 0.4rem 0;
+        }
+    }
+}
+
+.results {
+    text-align: center;
+    background-image: url('../assets/img/bg-10.jpg');
+    background-position: center;
+    color: $light;
+    padding: 8rem 0;
+
+    h2 {
+        font-size: 50px;
+        font-weight: 700;
+        margin: 2rem 0;
+
+        span {
+            background-color: $dark_green;
+            font-weight: 700;
+            padding: 0 1rem;
+        }
+    }
+
+    .number {
+        h5 {
+            color: $secondary;
+            font-weight: 600;
         }
     }
 }
